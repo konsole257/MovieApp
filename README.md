246d73b44e15cab77005b85c36682f06c58f23a5
konsole257 <konsole257@gmail.com>
2025-09-13 12:04:35 +0900

    commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Vite SSR + React + Redux + TypeScript
+
+- URL   
+https://konsole257.github.io/MovieApp/
--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import path from 'path';
 // https://vite.dev/config/
 export default defineConfig(() => {
   return {
-    base: '/', 
+    base: '/MovieApp/', 
     plugins: [react()],
     resolve: {
       alias: {
