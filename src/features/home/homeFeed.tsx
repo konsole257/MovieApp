2c86ccae6f6e61d1acf7de10166438638925f58f
konsole257 <konsole257@gmail.com>
2025-10-14 10:50:18 +0900

    詳細ページの修正

--- src/features/home/homeFeed.tsx
+++ src/features/home/homeFeed.tsx
@@ -18,7 +18,7 @@ export const HomeFeed = () => {
 
 	// if (loading) return <p>Loading...</p>;
 	if (error) return <p>Error: {error}</p>;
-	console.log(homeFeeds);
+
 	return (
 		<>
 			{homeFeeds.map(homeFeed => {
